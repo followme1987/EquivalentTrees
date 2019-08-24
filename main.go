c292090c36fc5f0749b672fedda85a4cac305737
yichen wu <wuyichen1987@gmail.com>
2019-08-24 20:58:23 +0100

    binary tree

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang.org/x/tour/tree"
+)
 
-func main(){
-	fmt.Println("hello world")
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	Walker(t, ch)
+	close(ch)
+}
+func Walker(t *tree.Tree, ch chan int) {
+	if t.Left != nil {
+		Walker(t.Left, ch)
+	}
+	ch <- t.Value
+	if t.Right != nil {
+		Walker(t.Right, ch)
+	}
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+
+	for i := range ch1 {
+		if i != <-ch2 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func main() {
+	result := Same(tree.New(2), tree.New(1))
+	fmt.Println(result)
 }
